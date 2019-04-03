43e0c1ab6b793ce3a212619c1df24165758e366f
edric <1@1.com>
2019-04-03 14:53:33 +0800

    code restructure

---
+++ FileUtil.h
@@ -0,0 +1,42 @@
+/*
+ * FileUtil.h
+ *
+ *  Created on: 2019年4月3日
+ *      Author: Edric
+ *      Brief:  文件相关操作工具
+ */
+
+#ifndef FILEUTIL_H
+#define FILEUTIL_H
+
+#include <string>
+#include <memory>
+#include <stdio.h>
+#include <iostream>
+#include <unistd.h>
+#include <time.h>
+#include <zlib.h>
+#include "zstr.cpp"
+
+class CFileUtil
+{
+public:
+    static const std::streamsize BUFF_SIZE = 1 << 16;
+
+public:
+    static int Compress(const std::string& rstrIn, const std::string& rstrOut);
+
+    static int Uncompress(const std::string& rstrIn, const std::string& rstrOut);
+
+    static int EncodeFile(const std::string& rstrSource, const std::string& rstrEncodeFile);
+
+    static int DecodeFile(const std::string& rstrEncodeFile, const std::string& rstrDecodeFile);
+
+private:
+    static void CatStream(std::istream& ris, std::ostream& ros);
+
+    static int ReverseStream(const std::string& rstrSource, const std::string& rstrOut);
+};
+
+#endif
+//FILEUTIL_H
