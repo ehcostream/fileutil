ec17ca5c233473570735b024c43b024ebcfe2a7b
edric <thought-x@163.com>
2019-04-30 17:40:59 +0800

    init project directory for dynamic link

---
+++ src/zlibwrapper/CMakeFiles/zlibwrapper2.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/zlibwrapper2.dir/strict_fstream.cpp.o"
+  "CMakeFiles/zlibwrapper2.dir/zio.cpp.o"
+  "libzlibwrapper2.pdb"
+  "libzlibwrapper2.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/zlibwrapper2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
