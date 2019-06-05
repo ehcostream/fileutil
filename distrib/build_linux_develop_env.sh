f2a417cecc94173b312b3ee2a314e76eba05734c
edric <ehcostream@gmail.com>
2019-06-05 07:03:14 +0000

    add execute rights to scripts

--- distrib/build_linux_develop_env.sh
+++ distrib/build_linux_develop_env.sh
@@ -3,8 +3,8 @@
 wget http://dl.bintray.com/boostorg/release/1.70.0/source/boost_1_70_0.tar.gz -O boost_1_70_0.tar.gz
 tar xvf boost_1_70_0.tar.gz -C .
 cd ./boost_1_70_0/
-sudo ./bootstrap.sh --prefix=/usr/local/
-sudo ./b2 toolset=gcc cxxflags="-std=c++11 -fPIC" --build-type=minimal --with-chrono --with-date_time --with-system --with-filesystem --with-thread link=static -j8 install
+./bootstrap.sh --prefix=/usr/local/
+./b2 toolset=gcc cxxflags="-std=c++11 -fPIC" --build-type=minimal --with-chrono --with-date_time --with-system --with-filesystem --with-thread link=static -j8 install
 
 #cloning the grpc and third parties
 cd ..
@@ -14,5 +14,5 @@ git submodule update --init
 
 #install grpc and third parties which are needed by grpc
 cd ..
-sudo ./run_distrib_cmake.sh
+./run_distrib_cmake.sh
 
